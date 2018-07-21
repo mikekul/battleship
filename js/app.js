cafaf5e8ba79b0ac6ca3862c1f52d5dfe2dbfb36
mikekul <michal.m.kulesza@gmail.com>
2018-07-21 17:16:14 +0200

    zadania z dnia 2 rozwiazane - w większości

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+// tablica dwuwymiarowa zawierajaca informacje o strzalach
+
 var shipsArray = [
     [' ', ' ', ' ', 'x', ' ', ' ', ' ', ' ', ' ', ' ',],
     [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',],
@@ -11,15 +13,16 @@ var shipsArray = [
     [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',],
 ];
 
-//console.log(shipsArray);
+//zmienne pomocnicze przechowujace strukture danych w html
 
 var html = '<table id="Battleships">'; //otwarcie tabeli
 
+//pętla przechodzaca po głownej tablicy
 for(var i = 0; i < shipsArray.length; i++){
   html += '<tr>';
 
   var row = shipsArray[i]; //tablica zagniezdzona
-
+  //petla przechodzaca po tablicach zagniezdzonych
   for(var j = 0; j < row.length; j++){
     html += '<td>';
     var element = row[j];
@@ -34,4 +37,4 @@ for(var i = 0; i < shipsArray.length; i++){
 
 html += '</table>'; //zamknięcie tabli
 
-document.write(html);
+document.write(html); //drukowanie elementu czyli html
